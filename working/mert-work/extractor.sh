d77d138766d9fd20769c8d9f16b14f12410dda61
NidhiSingh456 <115628508+NidhiSingh456@users.noreply.github.com>
2022-11-11 19:50:07 +0530

    Delete working/mert-work directory

--- working/mert-work/extractor.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd /home/mukesh/Desktop/MT/working/mert-work
-/home/mukesh/mosesdecoder/bin/extractor --sctype BLEU --scconfig case:true  --scfile run17.scores.dat --ffile run17.features.dat -r /home/mukesh/Desktop/MT/dev.true.en -n run17.best100.out.gz
